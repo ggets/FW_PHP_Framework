79b731b9fbfa241ca54c76f5578f37415dde2fcb
ggets <4815620+ggets@users.noreply.github.com>
2021-06-12 07:16:49 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# PHP Framework object
+
+  A Generic extendable PHP Object
+
+
+## Requirements:
+
+* PHP >7.3.0
---
+++ src/fw.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Ultimate Universal Web Project Framework
+ * @author  hkr GG
+ * @version 0.0.25
+ * @require PHP >=7.3.0
+ * @written PHP ==7.4.0
+ * @rev     12.06.2021
+ */
+namespace dreamflame\fwx{
+	class fw{
+		public static function obj($o=null){
+			return new class($o){
+				public function __isfwobj(){}
+				public function __call($m,$a){
+					if(isset($this->$m))return call_user_func_array($this->$m,$a);
+					else $this->m=$m;
+				}
+				public function __construct($o=null){
+					if(isset($o)){
+						$o=(object)$o;
+						if(!property_exists($o,'__p'))foreach($o as $k=>$v)$this->$k=$v;
+						elseif(property_exists($o,'__v'))foreach($o->__p as $v)$this->$v=$o->__v;
+						else foreach($o->__p as $k=>$v)$this->$k=$v;
+					}
+				}
+				public function __dump(){
+					$a=array();
+					foreach($this as $k=>$v)if(property_exists($this,$k))$a[$k]=$v;
+					return $a;
+				}
+				public function new($o=array()){
+					$o=(object)$o;
+					$n=clone $this;
+					foreach($o as $k=>$v)$n->$k=$v;
+					return $n;
+				}
+				public function isset($p=null){
+					return (($p===null)?null:property_exists($this,$p));
+				}
+			};
+		}
+		function e($s){echo($s);}
+		function v($s){var_dump($s);}
+		function vd($s){var_dump($s);die;}
+		function ve($s=null,$r=true){return var_export($s,$r);}
+		function n($m=null){
+			global $fw;
+			if(isset($fw))$fw->err(array('m'=>var_export($m,true)));
+		}
+		function elog($m=null){
+			global $fw;
+			if(isset($fw))$fw->err(array("m"=>$m,"o"=>"log"));
+		}
+		/* is_fwobj($o) */
+		function is_fwobj(&$o=null){return (isset($o)&&method_exists($o,'__isfwobj'));}
+		function pb($o=0,$l=0){debug_print_backtrace($o,$l);}
+		function backtrace_namespace(){
+			$t=array();//trace
+			$fns=array_map(
+				function($v){
+					return $v['function'];
+				},
+				debug_backtrace()
+			);
+			foreach($fns as $fn){
+				$f=new \ReflectionFunction($fn);
+				$t[]=array(
+					'function'=>$fn,
+					'namespace'=>$f->getNamespaceName()
+				);
+			}
+			return $t;
+		}
+	}
+}
